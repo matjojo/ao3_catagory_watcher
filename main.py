d607afd3ee8fbbbe23a831fc26544eb9fd3936fe
matjojo <8158314+matjojo@users.noreply.github.com>
2023-07-11 20:42:01 +0200

    fix that empty word-counts give an error (ao3 does not want to fix this)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime
 import time
+import sys
 from itertools import count
 
 import json
@@ -14,10 +15,10 @@ SLEEP_TIME = 0
 
 # login token, should probably be updated
 COOKIES = {
-    "_otwarchive_session": r"a3docUNaL2JNcmg0djFmSG44WC9VdXZ4aFE0bVhBd0VBMDBiaitIOGE0RWw5dWQ5eUwvckd6cXF1UmpVUXRlSzhOUHg1c1BoK3FrcUpvT0JZdkpJTVNEaUlnRmYveTFtcG00VDZhN2djK25EMktOY1NjZzliQUZJd3plbnc1bWErUWlzM1llemFCVVBzb1A5aUNYY0ZxRkx3ZDZkeXZGTXE3MFlsQURiS2VSZW1lcW9heW0wOEdUZXJ6VC9wT1NPNC9zbUpLdUwwcGc5cFdWRlRGNlQ0eVpYbkgxZEtVSGE4am1NNGVVcFpRTnpWU0xTY2Rpbm1VaGt4SVlHUFh1dWdVUzEyelNHTzZ6Z2V0bDNrNUJ2MHFOc3VobHRmMFBML1RkUlJMc3VDR2NxV1NiUlA2UGNMVmY3bldBSlNMR1EtLUZoWTczOXJNYk9NNjFrYXB1aW10a2c9PQ%3D%3D--4abda9dbb323f84f20fbc851c27324344276e0ae",
+    "_otwarchive_session": r"CqzsUY6rpq5kEQ8OylcFyPP1Mx7qk9OBnkzKp9sRNYNE8WPpw5BankCdx6AbsL9%2BjY%2FzGCwL7G1hYYgtSvcYnUFLgvO7bcKQKXPjpv%2B56oF%2FYImyUFMC6SFTuCCwRdK5uRSFujE7m7NMZBjErFRBOAGK4jiZu%2BVpSwiWoMwb3MTrD5bGKb7kU6Vch3fr2Kvo7BM5fmWyRrnQhwlpEEoidPumBcVjnGQFj5JgsMHE%2FtfiqCZKG5gdjgVcvfnlBFqnQITEINMtSkiewL4ej9KDDhElOSAXz7%2BFNjny6%2BmC9hleq2VhMTlKMBZJw1T6AW2bDWalb%2B%2Bb2YhNaWt3djZr%2BR8KyY981%2FweZ1K49H1EB4tE3QnIy1UnKTvUMavegAaholUPltJd4IRDw3cwmEDS5o%2BXfyaLRV%2FlwMIQnnVvCpO5zdRESL2Xaymr2InCaf%2F0HkWGLFkycQBaKDPpIqfFc9GamBtPq1lHdTfFmrrEVL6GBIXzkhxeCSsk7owrPyJDKGiH0pYGpsosPQeEJE6J9DceCRcnaxL%2B8sIHNeRABPMwGLOjomFqR2YWK0479w58rRluswwg6Eqx5bj4aQ%3D%3D--qLmUf%2Fi2E35fFZHV--8ysAQZ2l4gbTUgNJa2EVXA%3D%3D",
     "user_credentials": r"1",
     "view_adult": r"true",
-    "accepted_tos": "20180523"}
+    "accepted_tos": "20220110"}
 
 def get_url(base_url: str, page: int) -> str:
     return base_url + str(page)
@@ -68,7 +69,7 @@ def card_is_title_card(card: BeautifulSoup) -> bool:
 
 def story_cards(page_soup: BeautifulSoup) -> list[BeautifulSoup]:
     headings: list[BeautifulSoup] = page_soup.find_all("li", attrs={"role": "article"})
-    
+
     return [header for header in headings if card_is_title_card(header)]
 
 def get_urls_from_file() -> list[str]:
@@ -77,10 +78,10 @@ def get_urls_from_file() -> list[str]:
 
 class story():
     base_url = "https://archiveofourown.org"
-    
+
     def __init__(self, card: BeautifulSoup):
         self.card = card
-        
+
     def url(self) -> Optional[str]:
         try:
             return story.base_url + self.card.div.h4.a["href"]
@@ -92,7 +93,10 @@ class story():
 
     def words(self) -> Optional[int]:
         try:
-            return int(self.card.dl.find_all("dd", ["words"])[0].text.replace(",", "").replace(".", ""))
+            number_text:str = self.card.dl.find_all("dd", ["words"])[0].text.replace(",", "").replace(".", "")
+            if number_text == "":
+                return 0
+            return int(number_text)
         except (AttributeError, KeyError, IndexError, ValueError) as e:
             print(f"Could not extract wordcount from story card due to {e}. ({self.one_line_card_str()})")
 
@@ -122,23 +126,23 @@ if __name__ == '__main__':
     print("Reading urls with word counts dictionary...")
     urls_to_wordcount: dict[str, Optional[int]] = get_url_to_wordcount_dict()
     print(f"Read {len(urls_to_wordcount)} word counts.")
-    
+
     print("Now reading gallery urls...")
     name_to_url: dict[str, str] = get_gallery_url_dict()
     print(f"Read {len(name_to_url)} gallery urls")
-    
+
     new_urls: list[str] = []
     for url_name, base_url in name_to_url.items():
         print(f"Now searching for {url_name}...")
         known_urls_in_this_search = 0
         for page_number in count(start=1):
             url = get_url(base_url, page_number)
-    
+
             page_data = get_page(url)
             if page_data is None:
                 print(f"  page_data == none! (for page {url}) Exiting...")
                 break
-    
+
             soup = BeautifulSoup(page_data.content, 'html.parser')
             urls_found_on_this_page = 0
             known_urls_on_this_page = 0
@@ -160,23 +164,21 @@ if __name__ == '__main__':
                     known_urls_in_this_search += 1
                     known_urls_on_this_page += 1
                 urls_found_on_this_page += 1
-    
-    
+
+
             print(f"  Parsed page {page_number}, got {urls_found_on_this_page} urls. (changed: {len(new_urls)}, unchanged: {known_urls_in_this_search})")
-            
+
             if is_last_page(page_data):
                 print("  That was the last page.")
                 break
-            
+
             if known_urls_on_this_page == 20:
                 print(f"  We've found {known_urls_on_this_page} stories that haven't changed on this page. Assuming done...")
                 break
-            
+
             # don't want to scare the servers
             time.sleep(SLEEP_TIME)
-    
-    print("Saving known urls with word counts to file...")
-    save_obj_to_file(urls_to_wordcount)
+
     print("Saved known urls with word counts to file.")
     if new_urls:
         uniq_new_urls = set(new_urls)
@@ -190,5 +192,8 @@ if __name__ == '__main__':
             print("Failed to write urls:")
             for url in uniq_new_urls:
                 print(url)
-    
+
+    print("Saving known urls with word counts to file...")
+    save_obj_to_file(urls_to_wordcount)
+
     print(f"Finished check at {datetime.datetime.now()}")
